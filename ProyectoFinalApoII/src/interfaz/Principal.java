7de5c0ef8962f2bafc22c69ce3da967682ce5787
USUARIO <USUARIO@ASUS-CI5-X541U>
2018-05-03 14:41:25 -0500

    el diagrama

--- ProyectoFinalApoII/src/interfaz/Principal.java
+++ ProyectoFinalApoII/src/interfaz/Principal.java
@@ -18,7 +18,9 @@ public Principal() {
 
 public static void main(String[] args) {
 //hola
-	//ya estoy en linea
+	//ya les mande el diagrama, empiecen a hacer codigo,
+	//si van a trabajar en una classe escriban en esta parte: ej, estoy trabajando en la clase Principal
+	// esto sinifica que nadie puede trabajar sobre esa clase, saludos cuidense y me avisan si no saben o entienden el diagrama 
 	Principal p = new Principal();
 }
 }
