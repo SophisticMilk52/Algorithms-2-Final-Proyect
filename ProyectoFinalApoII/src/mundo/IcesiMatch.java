b9f9b55d09a79d8bb002903aae046d4327583035
vardo159 <36579511+vardo159@users.noreply.github.com>
2018-05-04 11:02:55 -0500

    Create IcesiMatch.java
    las relaciones no las entendí a plenitud. Me hace pensar que la clase usuario es una lista enlazada, o no sé si solamente es un contenedor, y siendo así, la cardinalidad me hace dudar

---
+++ ProyectoFinalApoII/src/mundo/IcesiMatch.java
@@ -0,0 +1,27 @@
+package mundo;
+
+import java.util.Comparator;
+
+public class IcesiMatch implements Comparator<Usuario>{
+	
+	private Usuario[] inicio,actual,pareja;
+	
+	public IcesiMatch() {
+		inicio=new Usuario[3];
+	}
+	
+	public void agregarUsuario(String s) {
+		
+	}
+	
+	public void eliminarUsuario(String s) {
+		
+	}
+
+	@Override
+	public int compare(Usuario u1, Usuario u2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
