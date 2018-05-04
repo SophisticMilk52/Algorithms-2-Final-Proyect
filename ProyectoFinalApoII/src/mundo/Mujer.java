630a4db849acfc1656cd3ea3f15f32057a6cfbdb
vardo159 <36579511+vardo159@users.noreply.github.com>
2018-05-04 10:56:53 -0500

    Create Mujer.java

---
+++ ProyectoFinalApoII/src/mundo/Mujer.java
@@ -0,0 +1,21 @@
+package mundo;
+
+public class Mujer extends Usuario{
+	public static final String MUJUER="Mujer";
+
+	public Mujer(String hETERO, String hOMO, String bI, int aciertos, char priPregunta, char segPregunta,
+			char triPregunta, char cuartPregunta, char quinPregunta, char sexPregunta, char sepPregunta,
+			char octPregunta, char novPregunta, char decPregunta, String foto, String nombre, String genero,
+			String idSexual, String carrera, double altura, int edad, int semeestre) {
+		super(hETERO, hOMO, bI, aciertos, priPregunta, segPregunta, triPregunta, cuartPregunta, quinPregunta, sexPregunta,
+				sepPregunta, octPregunta, novPregunta, decPregunta, foto, nombre, genero, idSexual, carrera, altura, edad,
+				semeestre);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int compareTo(Usuario u) {
+		return 0;
+	}
+	
+}
