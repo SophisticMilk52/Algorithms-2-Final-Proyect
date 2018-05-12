7a5a497d3eafd68834c5bb9b6b4d49012fd87ab9
vardo159 <36579511+vardo159@users.noreply.github.com>
2018-05-11 21:52:35 -0500

    Create PanelMenu.java

---
+++ ProyectoFinalApoII/src/interfaz/PanelMenu.java
@@ -0,0 +1,13 @@
+package interfaz;
+
+import javax.swing.JPanel;
+
+public class PanelMenu extends JPanel{
+
+	private MainWindow mw;
+	
+	public PanelMenu(Principal p) {
+		this.p=p;
+		setLayout(null);
+	}
+}
