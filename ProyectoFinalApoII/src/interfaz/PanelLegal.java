97407d3e98a9fa34ca576d5ba610004e7933e7a0
vardo159 <36579511+vardo159@users.noreply.github.com>
2018-05-11 21:35:27 -0500

    Update PanelLegal.java

--- ProyectoFinalApoII/src/interfaz/PanelLegal.java
+++ ProyectoFinalApoII/src/interfaz/PanelLegal.java
@@ -4,6 +4,9 @@ import javax.swing.JPanel;
 import javax.swing.JTextPane;
 
 import java.awt.Image;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Graphics;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -11,25 +14,38 @@ import javax.swing.JButton;
 
 public class PanelLegal extends JPanel{
 	
+	public static final String DE_ACUERDO="yes";
+	public static final String DESACUERDO="no";
+	
 	private JButton deAcuerdo;
 	private JButton desacuerdo;
 	private JTextPane contrato;
 	private Principal p;
+	private Image fondo;
+	private LogInWindow lw;
 	
 	public PanelLegal(Principal p) {
 		this.p=p;
 		
 		setLayout(null);
 		
+		lw=new LogInWindow(mw);
+		
+		fondo=new ImageIcon("images/fondoLegal.png").getImage();
+		
 		Icon icono=new ImageIcon("images/iconAgree.png");
 		deAcuerdo=new JButton();
 		deAcuerdo.setBounds(600,720, 120,100);
 		deAcuerdo.setIcon(icono);
+		deAcuerdo.setActionCommand(DE_ACUERDO);
+		deAcuerdo.addActionListener(this);
 		
 		icono=new ImageIcon("images/iconDisagree.png");
 		desacuerdo=new JButton();
 		desacuerdo.setBounds(300,720, 120, 100);
 		desacuerdo.setIcon(icono);
+		desacuerdo.setActionCommand(DESACUERDO);
+		desacuerdo.addActionListener(this);
 		
 		contrato=new JTextPane();
 		contrato.setBounds(210, 150, 600, 400);
@@ -41,6 +57,34 @@ public class PanelLegal extends JPanel{
 		add(desacuerdo);
 		add(deAcuerdo);	
 		add(contrato);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(fondo,0,0,getWidth(),getHeight(),this);
+		setOpaque(false);
+		super.paint(g);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent a) {
+		String comando=a.getActionCommand();
+		if(comando.equals(DE_ACUERDO)) {
+			mw.setVisible(false);
+			mostrarLogInWindow();		
+		}
+	}
+	
+	public void cerrarLogInWindow() {
+		lw.dispose();
+	}
+	
+	public void mostrarLogInWindow() {
+		lw.setVisible(true);
+	}
+	
+	public void cerrarSignUp() {
+		lw.pLogIn.cerrarSignUpWindow();
 	}
 
 }
