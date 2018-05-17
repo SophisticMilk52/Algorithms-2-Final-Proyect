1ca5f49ecb123122eaa421524198eeebdfd5c609
vardo159 <36579511+vardo159@users.noreply.github.com>
2018-05-16 22:28:52 -0500

    Create PanelPreguntas.java

---
+++ ProyectoFinalApoII/src/interfaz/PanelPreguntas.java
@@ -0,0 +1,12 @@
+package interfaz;
+
+import javax.swing.JPanel;
+
+public class PanelPreguntas extends JPanel{
+  
+  private Principal p;
+  
+  public PanelPreguntas(Principal p){
+    this.p=p;
+  }
+}
