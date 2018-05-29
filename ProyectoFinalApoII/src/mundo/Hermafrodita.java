dbd25d95d11e8459c97389ce92057966338ca509
alejandron00 <38502923+alejandron00@users.noreply.github.com>
2018-05-29 13:05:49 -0500

    hola 5

--- ProyectoFinalApoII/src/mundo/Hermafrodita.java
+++ ProyectoFinalApoII/src/mundo/Hermafrodita.java
@@ -9,8 +9,31 @@ import excepciones.SolitarioException;
 public class Hermafrodita extends Usuario{
 
 	//CONSTANTE//
+	/**
+	 * Constante de la clase Hermafrodita, String HERMA
+	 */
 	public static final String HERMA="Hermafrodita";
-
+	/**
+	 * Constructor de la clase Hermafrodita
+     * @param priPregunta /char respuesta de la pregunta 1
+	 * @param segPregunta /char respuesta de la pregunta 2
+	 * @param terPregunta /char respuesta de la pregunta 3
+	 * @param cuarPregunta /char respuesta de la pregunta 4
+	 * @param quinPregunta /char respuesta de la pregunta 5
+	 * @param sexPregunta /char respuesta de la pregunta 6
+	 * @param sepPregunta /char respuesta de la pregunta 7
+	 * @param octPregunta /char respuesta de la pregunta 8
+	 * @param novPregunta /char respuesta de la pregunta 9
+	 * @param decPregunta /char respuesta de la pregunta 10
+	 * @param foto /String ruta de la imagen
+	 * @param nombre /String nombre del usuario
+	 * @param genero /String genero del usuario
+	 * @param idSexual/ String id del usuario
+	 * @param carrera /String carrera del usuario
+	 * @param altura /double altura dle usuario
+	 * @param edad /int edad del usuario
+	 * @param semestre /int semestre del usuario
+	 */
 	public Hermafrodita(char priPregunta, char segPregunta, char triPregunta, char cuartPregunta,
 			char quinPregunta, char sexPregunta, char sepPregunta, char octPregunta, 
 			char novPregunta, char decPregunta, String foto, String nombre, 
@@ -21,6 +44,12 @@ public class Hermafrodita extends Usuario{
 	}
 
 	@Override
+	/**
+	 * El metodo emparejar de la clase Hermafrodita, este metodo me 
+	 * permite buscar la pareja de un hombre teniendo en cuenta las 
+	 * orientaciones y puntuacion en las repuestas
+	 * 
+	 */
 	public void emparejar(Usuario pareja) throws PopularException, SexualidadException, SolitarioException {
 		if(getPopularidad()>7) {
 			if(pareja.getCoincidencias()>6) {
--- ProyectoFinalApoII/src/mundo/Hombre.java
+++ ProyectoFinalApoII/src/mundo/Hombre.java
@@ -9,8 +9,32 @@ import excepciones.SolitarioException;
 public class Hombre extends Usuario{
 	
 	//CONSTANTE//
+	/**
+	 * Constante de la clase Hombre, String hombre
+	 */
 	public static final String HOMBRE="Hombre";
 	
+	/**
+	 * Constructor de la clase Hombre
+     * @param priPregunta /char respuesta de la pregunta 1
+	 * @param segPregunta /char respuesta de la pregunta 2
+	 * @param terPregunta /char respuesta de la pregunta 3
+	 * @param cuarPregunta /char respuesta de la pregunta 4
+	 * @param quinPregunta /char respuesta de la pregunta 5
+	 * @param sexPregunta /char respuesta de la pregunta 6
+	 * @param sepPregunta /char respuesta de la pregunta 7
+	 * @param octPregunta /char respuesta de la pregunta 8
+	 * @param novPregunta /char respuesta de la pregunta 9
+	 * @param decPregunta /char respuesta de la pregunta 10
+	 * @param foto /String ruta de la imagen
+	 * @param nombre /String nombre del usuario
+	 * @param genero /String genero del usuario
+	 * @param idSexual/ String id del usuario
+	 * @param carrera /String carrera del usuario
+	 * @param altura /double altura dle usuario
+	 * @param edad /int edad del usuario
+	 * @param semestre /int semestre del usuario
+	 */
 	public Hombre(char priPregunta, char segPregunta, char triPregunta, char cuartPregunta,
 			char quinPregunta, char sexPregunta, char sepPregunta, char octPregunta, 
 			char novPregunta, char decPregunta, String foto, String nombre, 
@@ -21,6 +45,12 @@ public class Hombre extends Usuario{
 	}
 
 	@Override
+	/**
+	 * El metodo emparejar de la clase Hombre, este metodo me 
+	 * permite buscar la pareja de un hombre teniendo en cuenta las 
+	 * orientaciones y puntuacion en las repuestas
+	 * 
+	 */
 	public void emparejar(Usuario pareja) throws PopularException, SexualidadException, SolitarioException {
 		if(getPopularidad()>7) {
 			if(pareja.getCoincidencias()>6) {
